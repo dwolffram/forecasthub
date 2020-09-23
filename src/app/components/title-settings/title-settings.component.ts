9701835e6a8200260a1b94d12de8f0c180ecf91b
whoohoo86 <whoohoo86@gmail.com>
2020-09-23 14:34:49 +0200

    fügt play/pause und stop button zur animation des forecastDates hinzu

--- src/app/components/title-settings/title-settings.component.ts
+++ src/app/components/title-settings/title-settings.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { TruthToPlotValue } from 'src/app/models/truth-to-plot';
 import { LookupService } from 'src/app/services/lookup.service';
-import { combineLatest, Observable } from 'rxjs';
+import { combineLatest, interval, noop, Observable, Subscription } from 'rxjs';
 import { tap, map } from 'rxjs/operators';
 import * as _ from 'lodash';
 import { ForecastDateLookup } from 'src/app/models/lookups';
 import { ForecastDateDisplayMode, ForecastDisplayMode, ForecastHorizonDisplayMode, ForecastPlotService } from 'src/app/services/forecast-plot.service';
 import { QuantileType } from 'src/app/models/forecast-to-plot';
-import { faAngleLeft, faAngleRight, faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faAngleLeft, faAngleRight, faArrowLeft, faArrowRight, faPause, faPlay, faStop } from '@fortawesome/free-solid-svg-icons';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-title-settings',
@@ -25,9 +26,13 @@ export class TitleSettingsComponent implements OnInit {
 
   currentDate = new Date();
 
+  isPlayingForecastDate: boolean = false;
   icons = {
     left: faAngleLeft,
     right: faAngleRight,
+    play: faPlay,
+    stop: faStop,
+    pause: faPause
   };
 
   constructor(private lookupService: LookupService, private stateService: ForecastPlotService) {
@@ -58,12 +63,16 @@ export class TitleSettingsComponent implements OnInit {
   }
 
   changeForecastHorizon(horizon: 1 | 2 | 3 | 4) {
+    if (this.isPlayingForecastDate) {
+      this.stopForecastDate();
+    }
     this.stateService.userDisplayMode = { $type: 'ForecastHorizonDisplayMode', horizon };
   }
 
   changeForecastDateByDir(dir: 'prev' | 'next', forecastDates: ForecastDateLookup, currentDate: moment.Moment) {
     const dateUpdate = forecastDates.getDateByDir(currentDate, dir);
     this.changeForecastDate(dateUpdate);
+    return dateUpdate;
   }
 
   changeDisplayMode(displayModeType: 'ForecastDateDisplayMode' | 'ForecastHorizonDisplayMode', maxDate: moment.Moment) {
@@ -76,6 +85,29 @@ export class TitleSettingsComponent implements OnInit {
     else {
       throw new Error(`Invalid displayMode type '${displayModeType}'. Unable to change display mode.`);
     }
+  }
+
+  player: Subscription;
+  forecastDateBeforePlayer: moment.Moment;
+  playPauseForecastDate(forecastDates: ForecastDateLookup, currentDate: moment.Moment) {
+    this.isPlayingForecastDate = !this.isPlayingForecastDate;
+    if (this.isPlayingForecastDate) {
+      this.forecastDateBeforePlayer = this.forecastDateBeforePlayer || moment(currentDate);
+
+      let d = currentDate;
+      this.player = interval(1000).subscribe((interval) => {
+        d = this.changeForecastDateByDir('prev', forecastDates, d);
+      });
+    } else {
+      this.player.unsubscribe();
+    }
+  }
+
+  stopForecastDate() {
+    this.isPlayingForecastDate = false;
+    this.player.unsubscribe();
+    this.changeForecastDate(this.forecastDateBeforePlayer);
+    this.forecastDateBeforePlayer = null;
   }
 
 }
